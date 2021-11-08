268195c0830a9e9fe7c419b46836375fd0c4e164
Nuno Oliveira <oliveiranunopt@gmail.com>
2021-11-08 10:56:59 +0000

    Insert index column

--- src/data_prep.py
+++ src/data_prep.py
@@ -4,7 +4,7 @@ from definitions import ROOT_DIR
 import tools
 
 # Import csv
-df = pd.read_csv(ROOT_DIR + '\\..\\files\\steam_games_original.csv')
+df = pd.read_csv(ROOT_DIR + '\\..\\files\\steam_games.csv')
 
 # Remove discount
 df = df.drop('discount_price', axis=1)
@@ -13,6 +13,7 @@ df = df.drop('discount_price', axis=1)
 df = df.drop(df[(df['types'] == 'bundle') | (df['types'] == 'sub')].index).reset_index(drop=True)
 df = df.drop('types', axis=1)
 
+bad_rows = []
 id_list = []
 for i, row in df.iterrows():
 
@@ -25,15 +26,21 @@ for i, row in df.iterrows():
             df.at[i, 'original_price'] = 0.0
 
     # Add app id to id_list
-    # split_url = row['url'].split("/")
-    # try:
-    #     app_index = split_url.index('app')
-    #     id_list.append(split_url[app_index+1])
-    # except:
-    #     print(split_url)
-        #print(df.loc[[i]])
+    split_url = row['url'].split("/")
+    try:
+        app_index = split_url.index('app')
+        id_list.append(split_url[app_index+1])
+    except:
+        bad_rows.append(i)
+
+# Remove bad rows
+for index in bad_rows:
+    df = df.drop(index)
+
+# Update index after operations
+df = df.reset_index(drop=True)
 
 # Create new column on Dataframe with all app id's
-# df['app_id'] = id_list
+df['app_id'] = id_list
 
-#print(df)
+print(df)

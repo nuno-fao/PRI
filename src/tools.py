767075e0b39cb8f628ec4b1803f5ecf8a9e92151
Nunation <oliveiranunopt@gmail.com>
2021-12-11 15:54:05 +0000

    yo

--- src/tools.py
+++ src/tools.py
@@ -1,4 +1,5 @@
 import requests
+import json
 def get_true_discount(df):
     x = 0
     for _, row in df.iterrows():
@@ -42,3 +43,22 @@ def get_n_reviews(appid, n=100):
         if len(response['reviews']) < 100: break
 
     return reviews
+
+def str_to_list():
+    f = open('src\\steam_games_prep.json')
+    data = json.load(f)
+    for entry in data:
+        if entry["popular_tags"]:
+            entry["popular_tags"] = entry["popular_tags"].split(",")
+        if entry["game_details"]:
+            entry["game_details"] = entry["game_details"].split(",")
+        if entry["languages"]:
+            entry["languages"] = entry["languages"].split(",")
+        if entry["genre"]:
+            entry["genre"] = entry["genre"].split(",")
+
+    with open('games.json', 'w') as outfile:
+        json.dump(data, outfile)
+    return
+
+str_to_list()
